509b45623d6b1c1ead6894c2d44509b2a788d4dd
prabhashiisurindi <prabhashiisurindi@gmail.com>
2020-05-06 20:43:58 +0530

    readme and DB file added

---
+++ Payments/Readme.md
@@ -0,0 +1,4 @@
+Student ID - IT18129236 
+Student Name - Isurindi H.G.P
+Group Weekend G01 S2(IT)
+PAF Group - S1012.8
---
+++ Payments/paf.sql
@@ -0,0 +1,33 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : LocalHost
+ Source Server Type    : MySQL
+ Source Server Version : 100406
+ Source Host           : localhost:3306
+ Source Schema         : paf
+
+ Target Server Type    : MySQL
+ Target Server Version : 100406
+ File Encoding         : 65001
+
+ Date: 06/05/2020 17:47:49
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for payment
+-- ----------------------------
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE `payment`  (
+  `paymentId` int(11) NOT NULL AUTO_INCREMENT,
+  `patientName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `amout` int(11) NULL DEFAULT NULL,
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `date` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`paymentId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 26 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
